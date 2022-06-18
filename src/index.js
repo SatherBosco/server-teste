c779fa0c0b0a33e3f30bdffe566549c38de3ba4d
Sather Bosco <satherbosco@hotmail.com>
2022-06-18 18:34:16 -0300

    https change 5

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 require('./app/controllers/index')(app);
 
 app.listen(3333);
