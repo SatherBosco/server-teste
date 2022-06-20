65c1ba8f06d0d186ddd1fb7902cdc7160ad0d577
Sather Bosco <satherbosco@hotmail.com>
2022-06-20 19:56:36 -0300

    cors3

--- src/index.js
+++ src/index.js
@@ -6,12 +6,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Add Access Control Allow Origin headers
+app.use(cors());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     next();
 });
+
+
+// app.use((req,res,next)=>{
+//     res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:4200');
+//     if(req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
 
 app.get('/', (req, res) => {
     return res.send({ msg: 'OK' });
