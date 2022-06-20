7682dfa51d2d8f0e1c24cbb8f1d7b9ad9f8c721f
Sather Bosco <satherbosco@hotmail.com>
2022-06-20 20:31:07 -0300

    cors7

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-    app.use(cors());
     next();
 });
 
